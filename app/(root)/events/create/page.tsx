3fba4663f868dcfa665abcca7991c1be3dd0ae30
Isaac <Scaiaaguinaga@gmail.com>
2024-09-12 23:42:48 -0700

    Added base styling for create event page and submission form

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,41 +1,9 @@
 "use client";
 
-import { IEvent } from "@/lib/mongodb/models/event.model";
-import React, { useState } from "react";
+import CreateEventForm from "@/components/CreateEventForm";
 
 const page = () => {
-  const [eventData, setEventData] = useState<Partial<IEvent>>({
-    title: "Test Event",
-    description: "This is a test to see if my database connection works",
-    location: "Woo Woo",
-    createdAt: new Date(),
-    imageUrl: "imageURL",
-    startDateTime: new Date(),
-    endDateTime: new Date(),
-    price: "$15.00",
-    isFree: false,
-    url: "eventURL",
-  });
-
-  const handleClick = async () => {
-    try {
-      const response = await fetch("/api/events", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json", // Inform the server about the content type
-        },
-        body: JSON.stringify(eventData),
-      });
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-
-  return (
-    <section>
-      <button onClick={handleClick}>Create Event</button>
-    </section>
-  );
+  return <CreateEventForm />;
 };
 
 export default page;
--- components/CreateEventForm.tsx
+++ components/CreateEventForm.tsx
@@ -1,21 +1,19 @@
+"use client";
+
 import { IEvent } from "@/lib/mongodb/models/event.model";
-import React, { useState } from "react";
+import { useState } from "react";
 
 const CreateEventForm = () => {
-  const [eventData, setEventData] = useState<IEvent>({
-    _id: "",
-    title: "",
-    description: "",
-    location: "",
-    createdAt: new Date(),
-    imageUrl: "",
+  const [eventData, setEventData] = useState<Partial<IEvent>>({
+    title: "Test Event",
+    description: "This is a test to see if my database connection works",
+    location: "Woo Woo",
+    imageUrl: "imageURL",
     startDateTime: new Date(),
     endDateTime: new Date(),
-    price: "",
+    price: "$15.00",
     isFree: false,
-    url: "",
-    // category: { _id: string; name: string };
-    // organizer: { _id: string; name: string };
+    url: "eventURL",
   });
 
   const handleClick = async () => {
@@ -34,7 +32,112 @@ const CreateEventForm = () => {
 
   return (
     <section>
-      <button onClick={handleClick}>Create Event</button>
+      {/* <h1>{JSON.stringify(eventData)}</h1> */}
+      <form className="grid grid-cols-2 gap-y-3 md:gap-y-6">
+        <div className="col-span-2 flex flex-col">
+          <label htmlFor="event-title" className="event-form-label">
+            Title
+          </label>
+          <input
+            id="event-title"
+            type="text"
+            placeholder="Title"
+            className="event-form-input w-[175px] md:w-[300px]"
+            onChange={(e) =>
+              setEventData({ ...eventData, title: e.target.value })
+            }
+          />
+        </div>
+
+        <div className="col-span-2 flex flex-col">
+          <label htmlFor="event-description" className="event-form-label">
+            Description
+          </label>
+          <textarea
+            id="event-description"
+            placeholder="Description"
+            className="event-form-input h-[150px] w-[250px] md:h-[200px] md:w-[400px]"
+            onChange={(e) =>
+              setEventData({ ...eventData, description: e.target.value })
+            }
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="event-location" className="event-form-label">
+            Location
+          </label>
+          <input
+            id="event-location"
+            type="text"
+            placeholder="Location"
+            className="event-form-input w-[175px] md:w-[300px]"
+            onChange={(e) =>
+              setEventData({ ...eventData, location: e.target.value })
+            }
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="event-image" className="event-form-label">
+            Image
+          </label>
+          <input id="event-image" type="file" className="pt-[3px]" />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="event-start-date" className="event-form-label">
+            Start Date
+          </label>
+          <input
+            id="event-start-date"
+            type="date"
+            placeholder="Start Date"
+            className="event-form-input w-[175px] md:w-[300px]"
+            // onChange={(e) =>
+            //   setEventData({ ...eventData, location: e.target.value })
+            // }
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="event-end-date" className="event-form-label">
+            End Date
+          </label>
+          <input
+            id="event-end-date"
+            type="date"
+            placeholder="End Date"
+            className="event-form-input w-[175px] md:w-[300px]"
+            // onChange={(e) =>
+            //   setEventData({ ...eventData, location: e.target.value })
+            // }
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="event-price" className="event-form-label">
+            Price
+          </label>
+          <input
+            id="event-price"
+            type="number"
+            placeholder="Price"
+            className="event-form-input w-[175px] md:w-[300px]"
+            onChange={(e) =>
+              setEventData({ ...eventData, price: e.target.value })
+            }
+          />
+        </div>
+      </form>
+
+      {/* Temp Submit Button */}
+      <button
+        onClick={handleClick}
+        className="mt-20 rounded-lg bg-primary p-2 text-3xl font-bold"
+      >
+        Create Event
+      </button>
     </section>
   );
 };
